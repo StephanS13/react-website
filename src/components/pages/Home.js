a7c7b31ec0d6d83d49a46d66292ff149062acf16
Stephane Simcic <simcic.stephane@gmail.com>
2021-01-26 14:46:02 +0800

    adds components

---
+++ src/components/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../../App.css';
+import HeroSection from '../HeroSection';
+import Cards from '../Cards.js';
+import Footer from '../Footer.js';
+
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
